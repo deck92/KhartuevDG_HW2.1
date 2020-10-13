3143d76327f0c11b3c4597caa028eab8628ef1ed
Dmitry Khartuev <deck92@yandex.ru>
2020-10-13 23:35:04 +0300

    first commit

---
+++ KhartuevDG_HW2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KhartuevDG_HW2.1
+//
+//  Created by Macbook on 13.10.2020.
+//  Copyright © 2020 tt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
